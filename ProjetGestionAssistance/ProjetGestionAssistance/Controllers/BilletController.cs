629310d28117c32004c896344a0aabe94276b37f
Charles <charles.andref@gmail.com>
2017-09-26 14:15:26 -0400

    ajout du controleur et de la vue pour créer un billet

---
+++ ProjetGestionAssistance/ProjetGestionAssistance/Controllers/BilletController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using ProjetGestionAssistance.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetGestionAssistance.Controllers
+{
+    public class BilletController : Controller
+    {
+        //Initialisation de la variable _context pour intéragir avec la base de données
+        private readonly ProjetGestionAssistanceContext _context;
+
+        public BilletController(ProjetGestionAssistanceContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var projetGestionAssistanceContext = _context.Billet.Include(b => b.Auteur).Include(b => b.Departement);
+            return View(await projetGestionAssistanceContext.ToListAsync());
+        }
+
+
+        public IActionResult Creation()
+        {
+            ViewData["AuteurId"] = new SelectList(_context.Compte, "Id", "Courriel");
+            ViewData["DepartementId"] = new SelectList(_context.Departement, "Id", "Id");
+            return View("Creation");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Creation([Bind("Id,Titre,Description,Etat,Image,Commentaires,AuteurId,DepartementId")] Billet billet)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(billet);
+                await _context.SaveChangesAsync();
+
+                //À CHANGER pour la vue des billets fait par Joel
+                return RedirectToAction("Index", "Billet");
+            }
+            ViewData["AuteurId"] = new SelectList(_context.Compte, "Id", "Courriel", billet.AuteurId);
+            ViewData["DepartementId"] = new SelectList(_context.Departement, "Id", "Id", billet.DepartementId);
+            return View(billet);
+        }
+
+
+    }
+}
