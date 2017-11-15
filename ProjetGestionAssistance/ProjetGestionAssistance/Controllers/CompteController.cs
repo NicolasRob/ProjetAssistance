0e0d88690332ef1bf89d1616f2b1856cb573aed3
NicolasRob <robillardnic@gmail.com>
2017-11-15 15:23:54 -0500

    Correction des accents

--- ProjetGestionAssistance/ProjetGestionAssistance/Controllers/CompteController.cs
+++ ProjetGestionAssistance/ProjetGestionAssistance/Controllers/CompteController.cs
@@ -12,10 +12,10 @@ using System.Reflection;
 
 namespace ProjetGestionAssistance.Controllers
 {
-    //Controlleur qui g�re les logins et les cr�ations de comptes
+    //Controlleur qui gére les logins et les créations de comptes
     public class CompteController : Controller
     {
-        //Initialisation de la variable _context pour int�ragir avec la base de donn�es
+        //Initialisation de la variable _context pour intéragir avec la base de données
         private readonly ProjetGestionAssistanceContext _context;
 
         public CompteController(ProjetGestionAssistanceContext context)
@@ -31,11 +31,11 @@ namespace ProjetGestionAssistance.Controllers
             return View();
         }
 
-        //Francis Par� 2017-09-27
-        //Action appel� quand on appuie sur le bouton connection
-        //Si la connection fonctionne, On set le SessionId avec son Id d'utilisateur, il sera maintenant consid�r� connect�
+        //Francis Paré 2017-09-27
+        //Action appelé quand on appuie sur le bouton connection
+        //Si la connection fonctionne, On set le SessionId avec son Id d'utilisateur, il sera maintenant considéré connecté
         //Puis, On le redige vers l'action Index dans le controlleur HomeController()
-        //**** A not� que l'utilisateur devra etre rediriger vers sa liste de billet lorsqu'elle sera impl�menter ****
+        //**** A noté que l'utilisateur devra etre rediriger vers sa liste de billet lorsqu'elle sera implémenter ****
         [HttpPost]
         public IActionResult Connection([Bind("Courriel")]string Courriel, [Bind("MotPasse")]string MotPasse)
         {
@@ -45,16 +45,16 @@ namespace ProjetGestionAssistance.Controllers
                 Compte tempCompte = _context.Compte.Include(cpt => cpt.Equipe).Where(cpt => cpt.Courriel == Courriel & cpt.MotPasse == MotPasse).AsEnumerable().SingleOrDefault(cpt => cpt.Courriel == Courriel & cpt.MotPasse == MotPasse);
                 if (tempCompte != null)
                 {
-                    // On v�rifie si le compte de l'utilisateur est actif
+                    // On vérifie si le compte de l'utilisateur est actif
                     if (!tempCompte.Actif)
                     {
                         // Le compte n'est pas actif, on le redirige vers le vue de login.
-                        // Puis, on affiche un message � l'utilisateur lui indiquant que son compte n'est pas actif
-                        ViewData["MessageErreurConnection"] = "Votre compte n'est pas activ�. Veuillez contacter un sup�rieur pour plus d'information.";
+                        // Puis, on affiche un message à l'utilisateur lui indiquant que son compte n'est pas actif
+                        ViewData["MessageErreurConnection"] = "Votre compte n'est pas activé. Veuillez contacter un supérieur pour plus d'information.";
                         return View("Login");
                     }
                     //Joel Lutumba 2017-10-01 -ajout
-                    //typeUtilisateur : variable dont la valeur est stock�e dans la session pour avoir le type de l'utilisateur connect�
+                    //typeUtilisateur : variable dont la valeur est stockée dans la session pour avoir le type de l'utilisateur connecté
                     string typeConnecte = "_Type";
                     string departementConnecte = "_Dep";
 
@@ -65,24 +65,24 @@ namespace ProjetGestionAssistance.Controllers
                     return RedirectToAction("Index", "Home");
                 }
                 // On affiche ce message d'erreur quand le courriel utilisateur ou le mot de passe saisie est incorrect.
-                ViewData["MessageErreurConnection"] = "Le courriel ou le mot de passe entr� est incorrect";
+                ViewData["MessageErreurConnection"] = "Le courriel ou le mot de passe entré est incorrect";
             }
             return View("Login");
         }
 
-        //Action qui affiche la page de cr�ation de compte
+        //Action qui affiche la page de création de compte
         public IActionResult AffichageCreation()
         {
-            //Ce ViewData sera utilis� dans le formulaire de cr�ation pour afficher une liste des ID des �quipes de la BD
+            //Ce ViewData sera utilisé dans le formulaire de création pour afficher une liste des ID des équipes de la BD
             ViewData["EquipeId"] = new SelectList(_context.Set<Equipe>(), "Id", "Nom");
             return View("Creation");
         }
 
-        //Action qui s'�x�cute quand on appuie sur le bouton Cr�ation du compte
-        //Les champs du formulaire POST seront automatiquement utilis�s pour cr�er un objet compte appropri�
-        //Cet objet sera ensuite ajouter � la BD
-        //Attention: Il faut avoir cr�er au moins une �quipe pour cr�er un utilisateur
-        //Sinon, la liste des �quipes sera vide et le formulaire ne sera jamais accept�
+        //Action qui s'éxécute quand on appuie sur le bouton Création du compte
+        //Les champs du formulaire POST seront automatiquement utilisés pour créer un objet compte approprié
+        //Cet objet sera ensuite ajouter à la BD
+        //Attention: Il faut avoir créer au moins une équipe pour créer un utilisateur
+        //Sinon, la liste des équipes sera vide et le formulaire ne sera jamais accepté
         public async Task<IActionResult> Creation([Bind("Id,Courriel,MotPasse,ConfirmationMotPasse,Nom,Prenom,Telephone,Type,Actif,EquipeId")] Compte compte)
         {
             compte.Type = 1;
@@ -93,26 +93,26 @@ namespace ProjetGestionAssistance.Controllers
                 compte.Actif = true;
                 if (ModelState.IsValid)
                 {
-                    //_contexte repr�sente la BD, .Add est une m�thode de DAO qui a �t� g�n�r� automatiquement
-                    //et compte est l'objet cr�� par le formulaire
+                    //_contexte représente la BD, .Add est une méthode de DAO qui a été généré automatiquement
+                    //et compte est l'objet créé par le formulaire
                     _context.Add(compte);
                     await _context.SaveChangesAsync();
                     //On redirige ensuite l'utilisateur vers la page de login
-                    //On utilise RedirectToAction plut�t que View pour �viter les doubles submit
+                    //On utilise RedirectToAction plutôt que View pour éviter les doubles submit
                     return RedirectToAction("Login");
                 }
             }
             else
-                ViewData["CourrielErreur"] = "Le courriel entr� est d�ja en utilisation";
-            //Si la cr�ation �choue, on redirige l'utilisateur vers la vue de Creation
-            //Cependant, il faut recr�er la liste des Id des �quipes puisqu'on a perdu le ViewData pr�c�dent
-            //lorsqu'on a appuy� sur le bouton de soumission.
+                ViewData["CourrielErreur"] = "Le courriel entré est déja en utilisation";
+            //Si la création échoue, on redirige l'utilisateur vers la vue de Creation
+            //Cependant, il faut recréer la liste des Id des équipes puisqu'on a perdu le ViewData précédent
+            //lorsqu'on a appuyé sur le bouton de soumission.
             ViewData["EquipeId"] = new SelectList(_context.Set<Equipe>(), "Id", "Nom", compte.EquipeId);
             return View();
         }
-        //Francis Par� : 2017-10-07
-        // Action qui d�connecte l'utilisateur de la session
-        // On clear l'�l�ment session, puis on redirige l'utilisateur � la page de connection
+        //Francis Paré : 2017-10-07
+        // Action qui déconnecte l'utilisateur de la session
+        // On clear l'élément session, puis on redirige l'utilisateur à la page de connection
         public IActionResult Deconnection()
         {
             HttpContext.Session.Clear();
