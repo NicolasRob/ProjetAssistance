130966f21e694553017ea0c743e6d6bf2f048b6b
NicolasRob <robillardnic@gmail.com>
2017-12-13 15:37:15 -0500

    Changements mineurs

--- ProjetGestionAssistance/ProjetGestionAssistance/Controllers/CompteController.cs
+++ ProjetGestionAssistance/ProjetGestionAssistance/Controllers/CompteController.cs
@@ -94,6 +94,7 @@ namespace ProjetGestionAssistance.Controllers
         //Cet objet sera ensuite ajouter à la BD
         //Attention: Il faut avoir créer au moins une équipe pour créer un utilisateur
         //Sinon, la liste des équipes sera vide et le formulaire ne sera jamais accepté
+        [HttpPost]
         public async Task<IActionResult> Creation([Bind("Id,Courriel,MotPasse,ConfirmationMotPasse,Nom,Prenom,Telephone,Type,Actif,EquipeId")] Compte compte)
         {
 
@@ -135,6 +136,8 @@ namespace ProjetGestionAssistance.Controllers
             return RedirectToAction("Login","Compte");
         }
 
+        //Affiche la page de gestion des comptes
+        //Utilise un ordre et un numéro de page pour faire fonctionner le système de tri
         public async Task<IActionResult> AfficherGestionCompte(String ordre, int? page)
         {
 
@@ -200,7 +203,7 @@ namespace ProjetGestionAssistance.Controllers
 
         }
 
-
+        //Affiche la page de modification d'un compte
         public async Task<IActionResult> AfficherModificationCompte(int? id, string ordre, int? page)
         {
             if (HttpContext.Session.GetInt32("_Id") == null || HttpContext.Session.GetInt32("_Type") < 3) {
@@ -224,6 +227,8 @@ namespace ProjetGestionAssistance.Controllers
             return View("ModificationCompte", compte);
         }
 
+        //Éxécute la modification d'un compte
+        [HttpPost]
         public async Task<IActionResult> ModifierCompte(int id, [Bind("Id,Courriel,MotPasse,ConfirmationMotPasse,Nom,Prenom,Telephone,Type,Actif,EquipeId")] Compte compte)
         {
             if (HttpContext.Session.GetInt32("_Id") == null || HttpContext.Session.GetInt32("_Type") < 3) {
@@ -259,6 +264,7 @@ namespace ProjetGestionAssistance.Controllers
             return View("ModificationCompte", compte);
         }
 
+        //Active ou désactive un compte
         public async Task<IActionResult> ModifierEtatCompte(int? id, string ordre, int? page)
         {
 
@@ -291,6 +297,7 @@ namespace ProjetGestionAssistance.Controllers
             return RedirectToAction("AfficherGestionCompte", new { ordre = ordre, page = page });
         }
 
+        //Fonction AJAX utilisé pour retourner la liste d'équipe d'un département
         public JsonResult ListeEquipeParDepartementId(int Id)
         {
             List<Equipe> listEquipe = new List<Equipe>(_context.Equipe.Where(e => e.DepartementId == Id));
